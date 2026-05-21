9f01de0eff67dd9f4e1fe00cc97b52cb370a9342
dilshanking6 <skhan585935@gmail.com>
2026-05-21 15:56:19 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.use('/api/chat', require('./server/routes/chat'));
 
 // Home Route
 app.get('/', (req, res) => {
-    res.send('Bharat-Adda Server is Running 🚀');
+    res.send(__dirname, 'public', 'index.html');
 });
 
 const Message = require('./server/models/Message');
