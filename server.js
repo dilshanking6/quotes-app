c0f3914b204f03892d607b083c1646df5f4d95d5
dilshanking6 <skhan585935@gmail.com>
2026-05-21 16:36:54 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -73,10 +73,7 @@ io.on('connection', (socket) => {
 });
 
 // MongoDB Connection
-mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
+mongoose.connect(process.env.MONGODB_URI)
 .then(() => console.log('MongoDB connected'))
 .catch(err => console.error('MongoDB connection error:', err));
 
